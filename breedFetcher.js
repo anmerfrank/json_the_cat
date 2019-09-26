aada0cd05fd7fadf6813c2b252d5fc0282bcacd9
Anne Franklin <anmerfrank@gmail.com>
2019-09-26 22:00:58 +0000

    Breed Fetcher, tested with Mocha/Chai. Paired programming with Brandon Miller.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,21 @@
-
-
 const request = require('request');
 
-const input = process.argv.slice(2);
-const webSearch = `https://api.thecatapi.com/v1/breeds/search?q=${input[0]}`;
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  
+  request((`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`), (error, response, body) => {
+    const data = JSON.parse(body);
+  
+    if (typeof data[0] === 'undefined') {
+      callback(error);
+  
+    } else
+  
+      callback(data[0].description, error);
+  });
+};
 
 
 
-
-request(webSearch, (error, response, body) => {
-
-  if (error) () => {
-    throw error('Your request has returned an error.')
-  }
-
-  const data = JSON.parse(body);
-
-
-  if (typeof data[0] === 'undefined') {
-    console.log('That breed does not exist!');
-
-  } else {
-
-  console.log(data[0].description);
-  }
-});
-
+module.exports = { fetchBreedDescription };
